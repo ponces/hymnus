89c1e3faba9cc640941c1f0911b82b3eeed50e21
ponces26 <ponces26@gmail.com>
2024-06-11 17:07:34 +0000

    feat: add song lyrics export

--- lib/screens/song.dart
+++ lib/screens/song.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:diacritic/diacritic.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:htmltopdfwidgets/htmltopdfwidgets.dart' as html2pdf;
+import 'package:path_provider/path_provider.dart';
+import 'package:share_extend/share_extend.dart';
 
 class SongScreen extends StatefulWidget {
   const SongScreen({
@@ -20,6 +27,12 @@ class _SongScreenState extends State<SongScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.share),
+            onPressed: () => _exportPdf(context),
+          ),
+        ],
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -37,5 +50,51 @@ class _SongScreenState extends State<SongScreen> {
         ],
       ),
     );
+  }
+
+  Future<void> _exportPdf(BuildContext context) async {
+    var tmpDir = (await getTemporaryDirectory()).path;
+    var filename = getFilename(widget.title);
+    var html = '''<!DOCTYPE html>
+    <html>
+      <head>
+        <title>${widget.title}</title>
+      </head>
+      <body>
+        <center>
+            <h1>${widget.title}</h1>
+        </center>
+        <br/>
+        <br/>
+        <font size="5">''';
+    html += widget.lyrics.replaceAll('\n', '<br/>');
+    html += '''</font>
+      </body>
+    </html>''';
+    var pdfFile = kIsWeb ? File(filename) : File('$tmpDir/$filename');
+    final pdfDoc = html2pdf.Document();
+    var widgets = await html2pdf.HTMLToPdf().convert(html);
+    pdfDoc.addPage(html2pdf.MultiPage(
+        maxPages: 10,
+        orientation: html2pdf.PageOrientation.portrait,
+        pageFormat: html2pdf.PdfPageFormat.a4,
+        build: (context) {
+          return widgets;
+        }));
+    await pdfFile.writeAsBytes(await pdfDoc.save());
+    ShareExtend.share(pdfFile.path, 'file');
+  }
+
+  String getFilename(String songTitle) {
+    String filename = '';
+    if ((songTitle.startsWith('CC') || songTitle.startsWith('HCC')) &&
+        songTitle.contains(' ')) {
+      filename = songTitle.split(' ')[0];
+    } else {
+      filename = removeDiacritics(
+        songTitle.replaceAll(RegExp('[-_,:;+!? ]'), ''),
+      );
+    }
+    return '$filename.pdf';
   }
 }
